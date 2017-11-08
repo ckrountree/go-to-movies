6911c33f1717c7b0d562e143faa9a8527f447bdd
ckrountree <jmail0207@gmail.com>
2017-11-08 13:24:33 -0800

    create search field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import camera from './camera.png';
 import './App.css';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const omdbKey = process.env.REACT_APP_OMDB_API_KEY;
 
 class App extends Component {
   constructor() {
     super();
     this.state= {
       movies: [],
-      search: '',
+      search: 'Dude',
       loading: false
     };
   }
@@ -19,10 +19,10 @@ class App extends Component {
 
   async loadSearch(search) {
     this.setState({ loading: true });
-    const response = await fetch(`http://www.omdbapi.com/${search}`);
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${omdbKey}&s=${search}`);
     const body = await response.json();
     this.setState({
-      movies: body.results,
+      movies: body.Search,
       loading: false
     });
   }
@@ -33,13 +33,18 @@ class App extends Component {
     });
   }
 
+
   render() {
     const { search, movies, loading } = this.state;
-    const choices = ['title', 'plot', 'release year'];
 
     const list = (
       <ul>
-        {movies.map(item => <li key={item.name}>{item.name}</li>)}
+        {movies.map(film => (
+          <li key={film.imdbID}>
+            {`${film.Title} (${film.Year})`}
+            <img src={film.Poster} alt="" /> 
+          </li>
+        ))} 
       </ul>
     );
 
@@ -49,22 +54,25 @@ class App extends Component {
       <section>
         <div className="App">
           <header className="App-header">
-            <img src={'./camera.png'} alt="" />
+            <img className="App-img" src={camera} alt="" />
             <h1 className="App-title">Roll That Footage</h1>
           </header>
         </div>
 
         <div>
-          {choices.map(choice => {
-            return <button key={choice} disabled={choice === search}
-              onClick={() => this.changeSearch(choice)}
-            >
-              {choice}
-            </button>;
-          })}
+          <label style={{ search, padding: '250px', textAlign: 'center' }}>
+            search:
+            <input name="search" value={search} 
+              changeSearch={({ target }) => this.changeSearch(target.value)} />
+          </label>
+        </div>
+        <img src={movies.poster} alt={movies.Title} />
+
+        <div>
           <div>{movies.length} {search}</div>
           {loading ? load : list}
         </div>
+
       </section>
     );
   }
