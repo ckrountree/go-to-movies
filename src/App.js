957a620240f12a1a8bd41ec2961b6474ec569bcd
ckrountree <jmail0207@gmail.com>
2017-11-07 22:21:31 -0800

    add loadSearch and changeSearch functions to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state= {
+      movies: [],
+      search: '',
+      loading: false
+    };
+  }
+  componentDidMount() {
+    this.loadSearch(this.state.search); 
+  }
+
+  async loadSearch(search) {
+    this.setState({ loading: true });
+    const response = await fetch(`http://www.omdbapi.com/${search}`);
+    const body = await response.json();
+    this.setState({
+      movies: body.results,
+      loading: false
+    });
+  }
+
+  changeSearch(search) {
+    this.setState({ search }, () => {
+      this.loadSearch(search);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={'./camera.png'} alt="" />
+          <h1 className="App-title">Roll That Footage</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
